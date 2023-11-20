60f300d1f2cedf3de56d943cdf552225bd2b05a1
Greyisheep <ibeawuchiclaret@gmail.com>
2023-11-20 04:30:59 -0800

    Second commit

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -5,6 +5,14 @@ def test_add():
     result = my_functions.add(1, 4)
     assert result == 5
     
+def test_add_strings():
+    result = my_functions.add("I like", " burgers")
+    assert result == "I like burgers"
+    
 def test_divide():
     result = my_functions.divide(10, 5)
     assert result == 2
+    
+def test_divide_by_zero():
+    with pytest.raises(ValueError):  
+        result = my_functions.divide(10, 0)
