7c5f24f9d6a7a1b044cad2e0e80f06822d03a21f
Greyisheep <ibeawuchiclaret@gmail.com>
2023-11-20 10:54:04 -0800

    Update

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -2,15 +2,16 @@ import pytest
 import math
 import source.shapes as shapes
 
+
 class TestCircle:
-    
+
     def setup_method(self, method):
         print(f"Setting up {method}")
         self.circle = shapes.Circle(10)
-        
+
     def teardown_method(self, method):
         print(f"Tearing down {method}")
-    
+
     def test_area(self):
         assert self.circle.area() == math.pi * self.circle.radius ** 2
 
@@ -18,5 +19,6 @@ class TestCircle:
         result = self.circle.perimeter()
         expected = 2 * math.pi * self.circle.radius
         assert result == expected
-        
-    
+
+    def test_not_same_area(self, my_rectangle):
+        assert self.circle.area() != my_rectangle.area()
--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,18 +1,38 @@
 import pytest
+import time
 import source.my_functions as my_functions
+
 
 def test_add():
     result = my_functions.add(1, 4)
     assert result == 5
-    
+
+
 def test_add_strings():
     result = my_functions.add("I like", " burgers")
     assert result == "I like burgers"
-    
+
+
 def test_divide():
     result = my_functions.divide(10, 5)
     assert result == 2
-    
+
+
 def test_divide_by_zero():
-    with pytest.raises(ValueError):  
+    with pytest.raises(ValueError):
         result = my_functions.divide(10, 0)
+        assert result == ValueError
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(10, 5)
+    assert result == 2
+  
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    assert my_functions.add(1, 2) ==3
+    
+@pytest.mark.xfail(reason="We cannot divide by zero")
+def test_divide_zero_error():
+    my_functions.divide(1, 0)
